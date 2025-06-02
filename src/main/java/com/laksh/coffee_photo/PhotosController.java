f0ce99664560f6eaae4791844cddf00b9eb39163
Laksh <lakshtaneja@yahoo.com>
2025-06-02 16:41:11 +0530

    add basic delete and post mapping

--- src/main/java/com/laksh/coffee_photo/PhotosController.java
+++ src/main/java/com/laksh/coffee_photo/PhotosController.java
@@ -2,14 +2,13 @@ package com.laksh.coffee_photo;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 @RestController
@@ -32,4 +31,21 @@ public class PhotosController {
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return db.get(id);
     }
+
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable String id){
+
+        Photo photo = db.remove(id);
+
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    }
+
+    @PostMapping("/photos")
+    public Photo create(@RequestBody Photo photo){ // jackson creates the java object with the empty constructor, sets file name with the setFileName setters
+        photo.setId(UUID.randomUUID().toString()); // id should be generated in the backend
+        db.put(photo.getId(), photo);
+        return photo;
+    }
+
 }
