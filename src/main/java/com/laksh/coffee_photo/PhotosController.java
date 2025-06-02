9f86a4ffb07a4906273e30ea862e73d2ce865f21
Laksh <lakshtaneja@yahoo.com>
2025-06-02 16:13:26 +0530

    add basic get mapping

---
+++ src/main/java/com/laksh/coffee_photo/PhotosController.java
@@ -0,0 +1,35 @@
+package com.laksh.coffee_photo;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+public class PhotosController {
+
+    private Map<String, Photo> db = new HashMap<>(){{
+        put("1", new Photo("1", "something.jpg"));
+    }};
+
+    @GetMapping("/photos")
+    public Collection<Photo> get(){
+        return db.values();
+    }
+
+    @GetMapping("/photos/{id}")
+    public Photo get(@PathVariable String id){
+
+        Photo photo = db.get(id);
+
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return db.get(id);
+    }
+}
