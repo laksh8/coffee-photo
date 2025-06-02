7f51fb07545702653a750e44105a33dd26a40f89
Laksh <lakshtaneja@yahoo.com>
2025-06-02 15:46:57 +0530

    Initial commit

---
+++ src/test/java/com/laksh/coffee_photo/CoffeePhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laksh.coffee_photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
