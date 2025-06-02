39a3b710e850e3e80334e93de1bd808d2ad763f7
Laksh <lakshtaneja@yahoo.com>
2025-06-02 16:13:17 +0530

    add basic photo model

---
+++ src/main/java/com/laksh/coffee_photo/Photo.java
@@ -0,0 +1,32 @@
+package com.laksh.coffee_photo;
+
+public class Photo {
+
+    private String id;
+
+    private String fileName;
+
+    // raw data
+
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
