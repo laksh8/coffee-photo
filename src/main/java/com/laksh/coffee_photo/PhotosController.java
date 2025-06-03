6362925169a5a0f923cd137bf93a24fc13179a82
Laksh <lakshtaneja@yahoo.com>
2025-06-03 19:47:47 +0530

    refactor to use photo service

--- src/main/java/com/laksh/coffee_photo/PhotosController.java
+++ src/main/java/com/laksh/coffee_photo/PhotosController.java
@@ -1,8 +1,6 @@
 package com.laksh.coffee_photo;
 
-
-import jakarta.servlet.annotation.MultipartConfig;
-import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,36 +8,33 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 
 @RestController
 public class PhotosController {
 
-    private Map<String, Photo> db = new HashMap<>(){{
-        put("1", new Photo("1", "something.jpg"));
-    }};
+    @Autowired PhotoService photoService;
 
     @GetMapping("/photos")
     public Collection<Photo> get(){
-        return db.values();
+
+        return photoService.get();
+
     }
 
     @GetMapping("/photos/{id}")
     public Photo get(@PathVariable String id){
 
-        Photo photo = db.get(id);
+        Photo photo = photoService.get(id);
 
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        return db.get(id);
+        return photoService.get(id);
     }
 
     @DeleteMapping("/photos/{id}")
     public void delete(@PathVariable String id){
 
-        Photo photo = db.remove(id);
+        Photo photo = photoService.remove(id);
 
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
@@ -48,14 +43,10 @@ public class PhotosController {
     @PostMapping("/photos")
     public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
 
-        Photo photo = new Photo();
+        Photo photo = photoService.save(file.getOriginalFilename(), file.getContentType() ,file.getBytes());
 
-        photo.setId(UUID.randomUUID().toString()); // id should be generated in the backend
-        photo.setFileName(file.getOriginalFilename());
-        photo.setData(file.getBytes());
-
-        db.put(photo.getId(), photo);
         return photo;
+
     }
 
 }
