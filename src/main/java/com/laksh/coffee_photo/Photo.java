c159d50dd7858e336c5466fcbedc3017254dfa95
Laksh <lakshtaneja@yahoo.com>
2025-06-03 19:46:39 +0530

    add content type field, getter, setter

--- src/main/java/com/laksh/coffee_photo/Photo.java
+++ src/main/java/com/laksh/coffee_photo/Photo.java
@@ -10,6 +10,8 @@ public class Photo {
     @NotEmpty
     private String fileName;
 
+    private String contentType;
+
     @JsonIgnore
     private byte[] data;
 
@@ -43,5 +45,13 @@ public class Photo {
 
     public void setData(byte[] data) {
         this.data = data;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
     }
 }
