456ce68e2a3df8f0fb1e5a29ca0606a009a22805
Laksh <lakshtaneja@yahoo.com>
2025-06-03 19:49:00 +0530

    refactor download controller add headers for download, use photo service to get data, id and filename handle null photo edge case

--- src/main/java/com/laksh/coffee_photo/DownloadController.java
+++ src/main/java/com/laksh/coffee_photo/DownloadController.java
@@ -1,23 +1,30 @@
 package com.laksh.coffee_photo;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class DownloadController {
 
+    @Autowired PhotoService photoService;
+
     @GetMapping("/download/{id}")
     public ResponseEntity<byte[]> download(@PathVariable String id) {
 
-        //TODO
-        byte[] data;
+        Photo photo = photoService.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        byte[] data = photo.getData();
 
         HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.valueOf(photo.getContentType()));
+
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(photo.getFileName()).build());
 
         return new ResponseEntity<>(data, headers, HttpStatus.OK);
 
